b81666d1c14135ccf72e5677fa991a01d8a5e26d
Svetlana Orlova <96784329+tudaiobratno@users.noreply.github.com>
2024-05-29 10:25:02 +0300

    add get_type func

--- Dictionaries/Prepare vocabulary.py
+++ Dictionaries/Prepare vocabulary.py
@@ -1,35 +1,52 @@
 import pandas as pd
-import numpy as np
-from Preparation_funct import get_type
+#import pymorphy2
+#It's possible to increase dictionary of words and types by adding new forms of particular words. It's easy to do with pymorphy lib
 
-'''
-#open file
-file = open('Type_dict.txt', 'w')
+def get_type(s):
+    temp = list(s)
+    used = dict()
+    ind = 0
+    res = ''
+    for let in temp:
+        if let == '-':
+            res += '-'
+        elif let not in used:
+            used[let] = str(ind)
+            res += str(ind) + '.'
+            ind += 1
+        else:
+            res += (used[let]) + '.'
 
-#Обработка данных
-df = pd.read_csv("rus_dict.csv")
-df = df.loc[:, ~df.columns.str.startswith('Unnamed')]
-df = df.drop("Часть речи", axis=1)
+    res = res.rstrip('.')
+    return res
 
-#Ищем похожие, выравниваем частоту
-u = df["Лемма"].unique()
-for elem in u:
-    if elem not in dict:
-        times = df['Лемма'].value_counts()[elem]
-        word = df.loc[df['Лемма'] == elem]
-        ipm = np.sum(word["Частота (ipm)"].values) / times
-        file.write(elem + ' ' + get_type(elem) + ' ' + str(ipm) + '\n')
-
-print("finished")
-file.close()
-'''
 
 file = open("New_types.txt", 'w')
 df = pd.read_csv("russian.utf-8", header=None)
 df.columns = ["Lemma"]
+df_freq = pd.read_csv("Type_dict.txt", sep=' ', header=None)
+df_freq.columns = ["word", "_", "ipm"]
 u = df["Lemma"].unique()
+#morph = pymorphy2.MorphAnalyzer()
 for elem in u:
-    file.write(elem + ',' + get_type(elem) + ',' + str(1) + '\n')
+
+    #exra for pymorhy. Works incorrectly
+    '''word = elem
+    parsed_word = morph.parse(word)[0]
+    word_forms = parsed_word.lexeme
+    all_forms = []
+    for form in word_forms:
+        all_forms.append(form.word)
+    all_forms = list(set(all_forms))'''
+
+    result = df_freq.loc[df_freq['word'] == elem]
+    k = result.shape
+    if k[0] != 0:
+        f = result.iloc[0, 2]
+        file.write(elem + ',' + get_type(elem) + ',' + str(f) + '\n') #frequency from the known data
+    else:
+        file.write(elem + ',' + get_type(elem) + ',' + str(0.01) + '\n')
+
 
 print("finished")
 file.close()
